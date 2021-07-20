14530efb8b0019a784fc1e7188edf0f8c388623f
Anastasiia Morozova <nm@yl-verkot.com>
2021-07-20 18:14:34 +0300

    added java faker

--- src/test/java/tests/RegistrationTestWithPageObjects.java
+++ src/test/java/tests/RegistrationTestWithPageObjects.java
@@ -1,16 +1,18 @@
 package tests;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
 public class RegistrationTestWithPageObjects {
 
     RegistrationPage registrationPage = new RegistrationPage();
+    Faker faker = new Faker();
 
     @Test
     void successfulRegistrationFormTest() {
         registrationPage.openPage();
-        registrationPage.typeFirstName("Anastasiia");
-        registrationPage.typeLastName("Morozova");
+        faker.name().firstName();
+        faker.name().lastName();
     }
 }
